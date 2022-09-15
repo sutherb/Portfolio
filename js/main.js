e9551b5a5ec007ea9e43e27a5bfb5300f6cd6f61
thecapper <thecapper@protonmail.com>
2022-09-15 18:29:04 -0400

    animations done, nav works fine too

--- js/main.js
+++ js/main.js
@@ -1,13 +1,39 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+
+    const html = document.getElementsByTagName('html')[0]
     const body = document.getElementsByTagName('body')[0]
     const footer = document.getElementsByTagName('footer')[0]
     const mobileMenu = document.getElementsByClassName('mobile-navbar')[0]
-    const mobileMenuBtn = document.getElementsByClassName('toggle-menu-button')[0]
-    
-    
+    const mobileMenuBtn = document.getElementsByClassName('toggle-menu-button')[0]    
     const navBtns = document.querySelectorAll('.mobile-menu .navbutton a')
-    console.log(navBtns)
+    const careerListItems = document.querySelectorAll('#career li')
+
+
+    const observer = new IntersectionObserver(entries => {
+        // Loop over the entries
+        entries.forEach(entry => {
+            // If the element is visible
+            if (entry.isIntersecting) {
+                // Add the animation class
+                if (entry.target.id === 'email_button') {
+                    entry.target.classList.add('bounce-in-top')
+                } else if (entry.target.tagName === 'LI') {
+                    entry.target.classList.add('bounce-in-top')
+                } else {
+                    entry.target.classList.add('focus-in-expand')
+                }
+            }
+        });
+    });
+
+    observer.observe(document.querySelector('#about'));
+    observer.observe(document.querySelector('#contact .button'));
+    careerListItems.forEach(li => {
+        observer.observe(li)
+    })
+    
+
 
     const toggleMobileMenu = () => {
         body.classList.toggle('blur')
@@ -21,18 +47,14 @@ document.addEventListener('DOMContentLoaded', () => {
         toggleMobileMenu()
     })
 
-    window.addEventListener('click', ({ target }) => {
-
-        if ((target === body || target === footer) & body.classList.contains('blur') ) {
+    window.addEventListener('click', ({target}) => {
+        if ((target === html || target === body || target === footer) & body.classList.contains('blur') ) {
             toggleMobileMenu()
-
         }
     });
 
     
-    //primitive, replace with mapping
     navBtns.forEach(navBtn => {
-        console.log(navBtn)
         navBtn.addEventListener('click', () => {
             toggleMobileMenu()
         })
