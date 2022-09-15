49158162dec96cd7b1fab814e96113993f3f6033
thecapper <thecapper@protonmail.com>
2022-09-15 12:50:01 -0400

    add animated text, mobile menu is good

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     const body = document.getElementsByTagName('body')[0]
+    const footer = document.getElementsByTagName('footer')[0]
     const mobileMenu = document.getElementsByClassName('mobile-navbar')[0]
     const mobileMenuBtn = document.getElementsByClassName('toggle-menu-button')[0]
     
     
-    const navBtns = document.getElementById("mobile-menu-list").getElementsByClassName('navbutton')
+    const navBtns = document.querySelectorAll('.mobile-menu .navbutton a')
+    console.log(navBtns)
 
     const toggleMobileMenu = () => {
         body.classList.toggle('blur')
+        footer.classList.toggle('blur')
         mobileMenu.classList.toggle('menu-open')
         mobileMenuBtn.classList.toggle('open')
     }
@@ -18,15 +21,22 @@ document.addEventListener('DOMContentLoaded', () => {
         toggleMobileMenu()
     })
 
-    
-    //primitive, replace with mapping
-    for (let i = 0; i <= navBtns.length; i++) {
-        navBtns[i].addEventListener('click', () => {
+    window.addEventListener('click', ({ target }) => {
+
+        if ((target === body || target === footer) & body.classList.contains('blur') ) {
             toggleMobileMenu()
-        })
-    }
+
+        }
+    });
 
     
+    //primitive, replace with mapping
+    navBtns.forEach(navBtn => {
+        console.log(navBtn)
+        navBtn.addEventListener('click', () => {
+            toggleMobileMenu()
+        })
+    })
 
 });
 
